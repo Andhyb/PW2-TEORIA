0034a6e6837cb79602d56bfb3c7f619e95381340
Andhy Chipana Mamani <achipanamam@unsa.edu.pe>
2025-05-04 21:17:37 -0500

    Modifique el script js para que valide los campos del formulario de contactame html para que no este vacio

--- Prac02/public/js/script.js
+++ Prac02/public/js/script.js
@@ -5,4 +5,25 @@ document.addEventListener("DOMContentLoaded", function () {
   if (footer) {
     footer.innerHTML += " | &copy; " + year;
   }
+
+// script para el formulario de contactame.html
+  const formulario = document.getElementById("form-contacto");
+  if (formulario) {
+    formulario.addEventListener("submit", function (e) {
+      e.preventDefault();
+      const nombre = document.getElementById("nombres").value.trim();
+      const correo = document.getElementById("correo").value.trim();
+      const asunto = document.getElementById("asunto").value.trim();
+      const contenido = document.getElementById("contenido").value.trim();
+      const mensaje = document.getElementById("mensaje-validacion");
+
+      if (!nombre || !correo || !asunto || !contenido) {
+        mensaje.style.color = "red";
+        mensaje.textContent = "Por favor, completa todos los campos requeridos.";
+      } else {
+        mensaje.style.color = "green";
+        mensaje.textContent = "Formulario completado correctamente (no se envía).";
+      }
+    });
+  }
 });
